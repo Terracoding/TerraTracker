4988a7a1bf8323462929ff71f934971df9d7c2ff
Dominic Wroblewski <domness@gmail.com>
2012-03-22 02:01:53 +0000

    Authenticate used before company

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,5 +1,7 @@
 class CompanyController < ApplicationController
-
+  
+  before_filter :authenticate_user!
+  
   def index
     current_company ? @current_company = current_company : redirect_to(new_company_path)
   end
