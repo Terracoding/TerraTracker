dbbce17e57339ac9c21db8c958da324afd214487
Dominic Wroblewski <domness@gmail.com>
2012-03-28 21:17:44 +0100

    Added test for tasks_count

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper'
 
 describe Company do
-  describe "validations" do
+  context :validations do
     it { should have_many(:users) }
     it { should have_many(:projects) }
   end
+
+  context :tasks_count do
+    it "should show 0 for no tasks" do
+      company = Factory.create(:company)
+      company.tasks_count.should == 0
+    end
+    it "should show tasks count" do
+      company = Factory.create(:company)
+      user = Factory.create(:user, :company => company, :owns_company => true)
+      project = Factory.create(:project, :company => company)
+      task = Factory.create(:task, :project_id => project.id)
+      company.tasks_count.should == 1
+    end
+  end
+
 end
