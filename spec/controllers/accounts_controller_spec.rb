c4e51887a860a992d623aedf9c3958c0304748f7
Dominic Wroblewski <domness@gmail.com>
2012-03-31 17:27:10 +0100

    Added spec for accounts controller

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 
 describe AccountsController do
 
+  context :index do
+    it "should require an authenticated user" do
+      get :index
+      response.should redirect_to(new_user_session_path)
+    end
+
+    it "should get the current company" do
+      @company = Factory.create(:company)
+      @user = Factory.create(:user, :company => @company, :owns_company => true)
+      sign_in @user
+      get :index
+      response.should be_successful
+    end
+  end
+
 end
