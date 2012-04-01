f52450b956975a2d3f76dc9ca66e5ed96e016764
Dominic Wroblewski <domness@gmail.com>
2012-04-02 00:42:57 +0100

    Basic styling for the pdfs

--- app/pdf_generators/report_generator.rb
+++ app/pdf_generators/report_generator.rb
@@ -5,15 +5,23 @@ class ReportGenerator < Prawn::Document
     @report = report
     display_header
     move_down 25
+    display_table
   end
 
   private
 
   def display_header
+    fill_color "777777"
+    text "Timesheet for #{@report.user.to_s}", size: 14, style: :bold, :align => :right
+    fill_color "000000"
     text @report.company.name, size: 24, style: :bold, :align => :left
-    text "#{@report.user.to_s} Timetracking Sheet", size: 15, style: :bold
     move_down 5
-    text "Report"
+  end
+
+  def display_table
+    t = make_table([['hello']])
+    t.draw
+    move_down 20
   end
 
 end
