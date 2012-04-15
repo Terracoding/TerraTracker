31e86a0e975bfeebb5667eb026b1ee9fe5eea4cb
Robert White <robert@terracoding.com>
2012-04-16 00:56:36 +0100

    Fixed bug with verbose timeformat inputs '0:05'

--- spec/models/timeslip_spec.rb
+++ spec/models/timeslip_spec.rb
@@ -40,5 +40,10 @@ describe Timeslip do
       @timeslip.hours = "0:5"
       @timeslip.hours.should == BigDecimal((5.to_f/6).to_s)
     end
+
+    it "works correctly with verbose time format input (0:05 = 5 minutes)" do
+      @timeslip.hours = "0:05"
+      @timeslip.hours.should == BigDecimal((5.to_f/60).to_s)
+    end
   end
 end
