00a3c49334734ad53cbf000a69549c60a3b3a2d5
Dominic Wroblewski <domness@gmail.com>
2012-04-17 15:52:35 +0100

    Shows recent timeslips on the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,7 @@ class DashboardController < ApplicationController
       @current_company = current_company
       @projects = @current_company.projects if @current_company.projects
       @subscription = Subscription.find_by_user_id(current_user.id)
+      @recent_timeslips = Timeslip.where(:user_id => current_user.id).limit(5).order("id DESC").reverse
     end
   end
   
