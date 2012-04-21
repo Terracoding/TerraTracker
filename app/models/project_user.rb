e072b1cea09fa304fff69a79ad3f1ee5164f3714
Robert White <robert@terracoding.com>
2012-04-21 22:41:57 +0100

    Can no longer add the same user to a project multiple times

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -1,4 +1,8 @@
 class ProjectUser < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+
+  validates_presence_of :project_id
+  validates_presence_of :user_id
+  validates_uniqueness_of :user_id, :scope => :project_id
 end
--- spec/models/project_user_spec.rb
+++ spec/models/project_user_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe ProjectUser do
   context :validations do
+    subject { ProjectUser.create!(:project_id => 1, :user_id => 1) }
     it { should belong_to(:project) }
     it { should belong_to(:user) }
+    it { should validate_presence_of(:project_id) }
+    it { should validate_presence_of(:user_id) }
+    it { should validate_uniqueness_of(:user_id).scoped_to(:project_id) }
   end
 end
