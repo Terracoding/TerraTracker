5df96214b7c4849084065ed796bdcb05c921d03a
Dominic Wroblewski <domness@gmail.com>
2012-04-29 23:51:43 +0100

    removed spec formatting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :rspec
-  config.formatter = 'documentation'
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
