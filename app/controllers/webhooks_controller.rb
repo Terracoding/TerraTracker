3f607d20c8c97971c27df6057cea1b4e127d26d0
Dominic Wroblewski <domness@gmail.com>
2012-05-14 14:01:16 +0100

    payload

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,10 +2,11 @@ class WebhooksController < ApplicationController
 
   def receive_payload
     if params[:payload]
-      respond_to { |format| format.json { render :json => "Got a payload?", :status => 200 }}
-      # if GoCardless.webhook_valid?(params[:payload])
-      #   respond_to { |format| format.json { render :json => "Got it!", :status => 200 }}
-      # end
+      if GoCardless.webhook_valid?(params[:payload])
+        respond_to { |format| format.json { render :json => "Got it!", :status => 200 }}
+      else
+        respond_to { |format| format.json { render :json => "Got a payload?", :status => 200 }}
+      end
     else
       respond_to { |format| format.json { render :json => "Didn't get a payload: #{params.inspect}", :status => 200 }}
     end
