b6e178dfccda20e0d9ab2414bfe42f728f16347b
Dominic Wroblewski <domness@gmail.com>
2012-05-21 02:07:01 +0100

    Started on resource confirmation tests

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -1,10 +1,9 @@
 require 'spec_helper'
 
 class ApiError
-  attr_accessor :status, :body
-  def initialize(status, body)
-    @status = status
-    @body = body
+  attr_accessor :status
+  def initialize()
+    @status = 401
   end
 end
 
@@ -30,12 +29,12 @@ describe SubscriptionsController do
     end
 
     it "should return other errors when there is an GoCardless API error" do
-      apiError = ApiError.new(401, "This is a random API error")
+      apiError = ApiError.new
       GoCardless::Subscription.stub(:find) { raise GoCardless::ApiError.new(apiError) }
       @subscription = FactoryGirl.create(:subscription, :company => @company)
       Subscription.stub(:find_by_company_id).and_return(@subscription)
       get :index
-      flash[:error].should == "GoCardless::ApiError [401] "
+      flash[:error].should_not be_empty
     end
 
     after :each do
@@ -44,7 +43,23 @@ describe SubscriptionsController do
   end
 
   context :confirm_subscription do
-    pending "not yet tested"
+    before :each do
+      @company = FactoryGirl.create(:company)
+      @user = FactoryGirl.create(:user, :company => @company, :owns_company => true)
+      sign_in @user
+    end
+
+    it "should show a message when confirming a bad resource" do
+      apiError = ApiError.new
+      GoCardless.stub(:confirm_resource).and_return(nil)
+      GoCardless.stub(:confirm_resource).and_raise(GoCardless::ApiError.new(apiError))
+      post :confirm_subscription
+      flash[:error].should_not be_empty
+    end
+
+    it "should" do
+      
+    end
   end
 
 end
