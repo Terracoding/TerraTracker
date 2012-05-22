c7e25c05227726e5f424a839b3d7ad1afad87956
Dominic Wroblewski <domness@gmail.com>
2012-05-22 10:14:04 +0100

    fixed tests

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -2,7 +2,7 @@ Given /^I am not logged in$/ do
   page.should_not have_content("Logout")
 end
 Given /^I am a user with the email "([^"]*)"$/ do |email|
-  User.create!(:email => email, :password => "password", password_confirmation: "password", :firstname => "John", :lastname => "Doe", :owns_company => true)
+  User.create!(:email => email, :password => "password", password_confirmation: "password", :firstname => "John", :lastname => "Doe", :owns_company => true, :sub_account => false, :company_admin => true)
 end
 Given /^I am signed in as user "([^"]*)"$/ do |email|
   steps %Q{
--- features/step_definitions/company_user_steps.rb
+++ features/step_definitions/company_user_steps.rb
@@ -3,10 +3,11 @@ Given /^I have a company user "([^"]*)"$/ do |firstname|
     Given I am on the company page
     When I follow "Add User"
     And I fill in the following:
-      | First Name            | #{firstname}                  |
-      | Last Name             | Doe                           |
-      | Email                 | #{firstname.downcase}doe@example.com   |
-      | Password              | password                      |
+      | First Name            | #{firstname}                            |
+      | Last Name             | Doe                                     |
+      | Email                 | #{firstname.downcase}doe@example.com    |
+      | Password              | password                                |
+    And I uncheck "Company Management"
     And I press "Create Company User"
     Then I should see "You have successfully added #{firstname} Doe to your company."
   }
