c55bbe250a596c7d83a27447e9bdea9c5f864217
Dominic Wroblewski <domness@gmail.com>
2012-05-31 15:49:25 +0100

    Pretty much blank developer controller spec

--- spec/controllers/developer_controller_spec.rb
+++ spec/controllers/developer_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe DeveloperController do
+  before(:each) do
+    @company = FactoryGirl.create(:company)
+    @user = FactoryGirl.create(:user, :company => @company, :owns_company => true)
+    sign_in @user
+  end
+
+  context :index do
+    it "should load the developer page" do
+      get :index
+      response.should be_success
+    end
+  end
+
+  context :new do
+    it "should load the create application page" do
+      get :new
+      response.should be_success
+    end
+  end
 
 end
