d22b10804c1559bc19994d721db7c7dd888cccaf
Dominic Wroblewski <domness@gmail.com>
2012-05-31 18:05:50 +0100

    developer specs

--- spec/controllers/developer_controller_spec.rb
+++ spec/controllers/developer_controller_spec.rb
@@ -7,6 +7,10 @@ describe DeveloperController do
     sign_in @user
   end
 
+  after(:each) do
+    sign_out @user
+  end
+
   context :index do
     it "should load the developer page" do
       get :index
@@ -35,17 +39,40 @@ describe DeveloperController do
       DeveloperApplication.stub(:new) { application }
       post :create
       response.should redirect_to(developer_index_path)
-      sign_out @user
+    end
+
+    it "should show a success message when saving a new application" do
+      application = FactoryGirl.create(:developer_application, :user => @user)
+      DeveloperApplication.stub(:new) { application }
+      post :create
+      flash[:notice].should == "The application was successfully created."
     end
 
     it "should render new if there was an error saving the application" do
-      
+      application = FactoryGirl.create(:developer_application, :user => @user)
+      DeveloperApplication.stub(:new) { application }
+      application.stub(:save).and_return(false)
+      post :create
+      response.should render_template(:new)
     end
   end
 
-  context :edit do
-    it "should be able to edit applications" do
-      
+  context :update do
+    before(:each) do
+      @application = FactoryGirl.create(:developer_application, :user => @user)
+    end
+
+    it "should redirect to developer applications path after updating" do
+      put :update, :id => @application.id
+      flash[:notice].should == "The application was successfully updated."
+      response.should redirect_to(developer_index_path)
+    end
+
+    it "should render edit template if not updated attributes" do
+      DeveloperApplication.should_receive(:find).and_return(@application)
+      @application.stub(:update_attributes).and_return(false)
+      put :update, :id => @application.id
+      response.should render_template(:edit)
     end
   end
 end
