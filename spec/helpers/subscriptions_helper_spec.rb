75a139fad555bad8ef84cf0a64fda9a7f9de3b58
Dominic Wroblewski <domness@gmail.com>
2012-06-23 15:42:18 +0100

    Fixed subscriptions test

--- spec/helpers/subscriptions_helper_spec.rb
+++ spec/helpers/subscriptions_helper_spec.rb
@@ -6,7 +6,8 @@ describe SubscriptionsHelper do
     it "should true if the current company plan is the same" do
       user = FactoryGirl.create(:user)
       plan = Plan.new(:title => "title")
-      @current_company = FactoryGirl.build(:company, :user => user)
+      @current_company = FactoryGirl.build(:company)
+      user.update_attribute(:company, @current_company)
       @current_company.stub(:plan).and_return(plan)
       current_plan(plan).should be_true
     end
@@ -14,7 +15,8 @@ describe SubscriptionsHelper do
     it "should false if the current company plan is not the same" do
       user = FactoryGirl.create(:user)
       plan = Plan.new(:title => "title")
-      @current_company = FactoryGirl.build(:company, :user => user)
+      @current_company = FactoryGirl.build(:company)
+      user.update_attribute(:company, @current_company)
       current_plan(plan).should be_false
     end
   end
